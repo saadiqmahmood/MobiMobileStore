ca19f1d661677b4bb3090438f6ee5b08cf938b43
saadiqmahmood <93130794+saadiqmahmood@users.noreply.github.com>
2022-05-16 09:25:11 +0100

    Add files via upload

---
+++ js/myform.js
@@ -0,0 +1,33 @@
+// JAVASCRIPT FORM VALIDATION
+
+// Creation of function called validateform
+function validateform() 
+    //Open function
+{
+    // Three variables created, for each field (name, email and comment)
+    var name=document.myform.name.value;
+    var email=document.myform.email.value;
+    var comment=document.myform.comment.value;
+    var address=document.myform.address.value;
+
+    //  IF statement created. Contains three sections:
+    // For name,
+    if (name==null || name==""){
+        alert("Please enter name");
+        return false;
+    }
+    // Email
+    else if (email==null || email==""){
+        alert("Please enter email");
+        return false;
+    }
+    // and Comment.
+    else if (comment==null || comment==""){
+        alert("Please enter comment");
+        return false;
+    }
+    else if (address==null || address==""){
+        alert("Please enter address")
+    }
+    // Close function
+}
